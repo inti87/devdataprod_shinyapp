8c10658dc9b85110031a98f54f62b1eb730970fc
Marko Intihar <intihar.marko@gmail.com>
2021-01-07 13:49:01 +0100

    Added diamond price prediction model

--- diamonds_shiny_app/server.R
+++ diamonds_shiny_app/server.R
@@ -14,11 +14,37 @@ library(dplyr)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
+    # Build model for diamond price prediction 
+    model <- lm(price ~ carat + color + cut, data = diamonds)
+    
+    # Predict diamond price using diamond features (provided by user)
+    modelpred <- reactive({
+        caratInput <- input$carat_user
+        colorInput <- input$color_user
+        cutInput   <- input$cut_user
+        
+        predict(model, newdata = data.frame(carat = caratInput,
+                                            color = colorInput,
+                                            cut = cutInput))
+    })
+    
+    # Predicted price (numeric value)
+    output$pred <- renderText({
+        modelpred()
+    })
+    
+    # Diamonds prices plot (diamonds data set) & predicted value (based on user input)
     output$plot <- renderPlot({
         
         # select number of diamonds based on input$nr_diamonds from ui.R
         df <- diamonds %>% 
             sample_n(size = input$nr_diamonds, replace = F)
+        
+        # data frame for predicted price and diamond features (user input)
+        df.pred <- data.frame(price = modelpred(),
+                              carat = input$carat_user,
+                              color = input$color_user,
+                              cut = input$cut_user)
         
         # draw diamonds price scatter plot 
         df %>% 
@@ -27,6 +53,14 @@ shinyServer(function(input, output) {
                        color = color,
                        shape = cut)) +
             geom_point(size = 3) +
+            geom_point(data = df.pred,
+                       aes(x = carat, y = price),
+                       color = "brown",
+                       size = 10) +
+            annotate(geom = "text", x = df.pred$carat, y = df.pred$price + 2000,
+                     label = "Your diamond price!",
+                     color = "brown",
+                     size = 7, face = "bold") +
             xlab("Diamond weight in carats") +
             ylab("Diamond price in USD") +
             ggtitle("Diamond prices") +
--- diamonds_shiny_app/ui.R
+++ diamonds_shiny_app/ui.R
@@ -22,12 +22,39 @@ shinyUI(fluidPage(
                         "How many diamonds (from the data) should be plotted?",
                         min = 0,
                         max = 50000,
-                        value = 10000)
+                        value = 10000),
+            sliderInput("carat_user",
+                        "What is your diamond weight (in carat)?",
+                        min = 0,
+                        max = 5,
+                        value = 2.5,
+                        step = 0.01),
+            radioButtons("color_user",
+                         "What is your diamond color (from J (worst) to D (best))?",
+                         choices = c("D" = "D",
+                                     "E" = "E",
+                                     "F" = "F",
+                                     "G" = "G",
+                                     "H" = "H",
+                                     "I" = "I",
+                                     "J" = "J"), 
+                         selected = "G"),
+            radioButtons("cut_user",
+                         "What is your diamond cut?",
+                         choices = c("Ideal" = "Ideal",
+                                     "Premium" = "Premium",
+                                     "Very Good" = "Very Good",
+                                     "Good" = "Good",
+                                     "Fair" = "Fair"),
+                         selected = "Very Good")
+            
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("plot", width = "100%")
+            plotOutput("plot", width = "100%"),
+            h3("Predicted diamond price in USD from lm model:"),
+            textOutput("pred")
         )
     )
 ))
