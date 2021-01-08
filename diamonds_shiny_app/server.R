99afdb3541c716722eb4456d4b5f02d1c88d7809
Marko Intihar <intihar.marko@gmail.com>
2021-01-08 06:36:45 +0100

    Shiny app finished, some additional code still commented

--- diamonds_shiny_app/server.R
+++ diamonds_shiny_app/server.R
@@ -1,17 +1,17 @@
+# Predict diamond price - ShinyApp - server engine (server.R)
 #
 # This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# author: Marko Intihar
 
+
+# load essential libraries
 library(shiny)
 library(ggplot2)
 library(dplyr)
 
-# Define server logic required to draw a histogram
+# Define server logic required to create prediction model, draw plot & make price prediction
 shinyServer(function(input, output) {
 
     # Build model for diamond price prediction 
@@ -19,34 +19,41 @@ shinyServer(function(input, output) {
     
     # Predict diamond price using diamond features (provided by user)
     modelpred <- reactive({
+        # input diamond features
         caratInput <- input$carat_user
         colorInput <- input$color_user
         cutInput   <- input$cut_user
         
+        # model prediction (diamond price)
         predict(model, newdata = data.frame(carat = caratInput,
                                             color = colorInput,
                                             cut = cutInput))
     })
     
-    # Predicted price (numeric value)
-    output$pred <- renderText({
-        modelpred()
+    # Predicted price (numeric value) - rounded on 100 USD
+    # output$pred <- renderText({
+    #     round(modelpred() / 100, 0) * 100
+    # })
+    
+    # Predicted price as string (added USD)
+    output$pred_USD <- renderText({
+        paste(round(modelpred() / 100, 0) * 100, "USD", sep = " ")
     })
     
-    # Diamonds prices plot (diamonds data set) & predicted value (based on user input)
+    # Diamonds prices plot (diamonds data set) & predicted diamond price (based on user inputs)
     output$plot <- renderPlot({
         
-        # select number of diamonds based on input$nr_diamonds from ui.R
+        # select number of diamonds (for the plot) based on user input
         df <- diamonds %>% 
             sample_n(size = input$nr_diamonds, replace = F)
         
-        # data frame for predicted price and diamond features (user input)
+        # data frame for predicted price and diamond features (user inputs)
         df.pred <- data.frame(price = modelpred(),
                               carat = input$carat_user,
                               color = input$color_user,
                               cut = input$cut_user)
         
-        # draw diamonds price scatter plot 
+        # create diamonds price scatter plot (actual diamonds & predicted diamond price)
         df %>% 
             ggplot(aes(x = carat, 
                        y = price,
@@ -55,15 +62,16 @@ shinyServer(function(input, output) {
             geom_point(size = 3) +
             geom_point(data = df.pred,
                        aes(x = carat, y = price),
-                       color = "brown",
+                       color = "brown1",
                        size = 10) +
             annotate(geom = "text", x = df.pred$carat, y = df.pred$price + 2000,
-                     label = "Your diamond price!",
-                     color = "brown",
+                     label = "Your diamond!",
+                     color = "brown1",
                      size = 7, face = "bold") +
+            scale_color_grey() +
             xlab("Diamond weight in carats") +
             ylab("Diamond price in USD") +
-            ggtitle("Diamond prices") +
+            ggtitle("Diamond prices (actual diamonds & your diamond's price prediction)") +
             theme(plot.title = element_text(size = 24),
                   axis.title = element_text(size = 20),
                   axis.text = element_text(size = 16),
--- diamonds_shiny_app/ui.R
+++ diamonds_shiny_app/ui.R
@@ -1,44 +1,53 @@
+# Predict diamond price - ShinyApp - user-interface (ui.R)
 #
 # This is the user-interface definition of a Shiny web application. You can
 # run the application by clicking 'Run App' above.
 #
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# author: Marko Intihar
 
-library(shiny)
 
-# Define UI for application that draws a histogram
+# load libraries
+library(shiny) 
+
+# Define UI for application that builds a diamond price prediction model & 
+# creates diamonds plot and predicts users's diamond price
 shinyUI(fluidPage(
 
     # Application title
     titlePanel("Diamond price (prediction)"),
 
-    # Sidebar with a slider input for number of bins
+    # Sidebar with sliders & radio buttons (for user input data - diamond features)
     sidebarLayout(
         sidebarPanel(
+            
+            # user input - control how many diamonds are plotted (from diamonds data set - ggplot2)
             sliderInput("nr_diamonds",
                         "How many diamonds (from the data) should be plotted?",
                         min = 0,
                         max = 50000,
                         value = 10000),
+            
+            # user input - weight of selected diamond (from the user) in carats
             sliderInput("carat_user",
-                        "What is your diamond weight (in carat)?",
+                        "What is your diamond's weight (in carats)?",
                         min = 0,
                         max = 5,
                         value = 2.5,
                         step = 0.01),
+            
+            # user input - color of selected diamond (from the user) - color options given
             radioButtons("color_user",
-                         "What is your diamond color (from J (worst) to D (best))?",
-                         choices = c("D" = "D",
+                         "What is your diamond's color (from J (worst) to D (best))?",
+                         choices = c("D  (best color)" = "D",
                                      "E" = "E",
                                      "F" = "F",
                                      "G" = "G",
                                      "H" = "H",
                                      "I" = "I",
-                                     "J" = "J"), 
+                                     "J (worst color)" = "J"), 
                          selected = "G"),
+            
+            # user input - cut of selected diamond (from the user) - cut options given
             radioButtons("cut_user",
                          "What is your diamond cut?",
                          choices = c("Ideal" = "Ideal",
@@ -46,15 +55,24 @@ shinyUI(fluidPage(
                                      "Very Good" = "Very Good",
                                      "Good" = "Good",
                                      "Fair" = "Fair"),
-                         selected = "Very Good")
+                         selected = "Very Good"),
+            
+            # master submit button - to run server calculations when inputs are defined
+            submitButton("Submit")
             
         ),
 
-        # Show a plot of the generated distribution
+        # Show a plot of diamond prices (actual diamonds & user's diamond's price prediction) &
+        # show predicted diamond price in USD (in numeric form)
         mainPanel(
             plotOutput("plot", width = "100%"),
-            h3("Predicted diamond price in USD from lm model:"),
-            textOutput("pred")
+            h3("Your diamond's predicted price (in USD) from simple linear model:"),
+            textOutput("pred_USD"),
+            # CSS styling for price prediction (shown as text)
+            tags$head(tags$style("#pred_USD{color: red;
+                                 font-size: 40px;
+                                 font-style: bold;
+                                 }"))
         )
     )
 ))
